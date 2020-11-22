93d33fd090f4fa913bd4a5bf15d57a9e659313c1
carlastjuste <carlastjuste@users.noreply.github.com>
2020-11-21 19:15:45 -0500

    update readme.md

---
+++ Script/script.js
@@ -0,0 +1,32 @@
+var date = new Date();
+var dayId = date.getDay();
+var month = date.getMonth();
+var day = date.getDate();
+
+var dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+$("#currentDay").html(dayName[dayId] + ", " + monthName[month] + " " + getDayWithSuffix());
+
+
+function getDayWithSuffix (Day) {
+    switch (day) {
+        case 1:
+        case 21:
+        case 31:
+            return "" + day + "st";
+
+        case 2:
+        case 22:
+            return "" + day + "nd";
+
+        case 3:
+        case 23:
+            return "" + day + "rd";
+
+        default:
+            return "" + day + "th";
+}
+}
+
+
