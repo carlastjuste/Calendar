925349f6f05eac05de9db5cc94e7de4b22ba30ec
carlastjuste <carlastjuste@users.noreply.github.com>
2020-11-21 23:08:55 -0500

    Implement time block generation

--- Script/script.js
+++ Script/script.js
@@ -3,6 +3,7 @@ var dayId = date.getDay();
 var month = date.getMonth();
 var day = date.getDate();
 
+console.log(date);
 var dayName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -31,19 +32,57 @@ function getDayWithSuffix (Day) {
 }
 
 function displayTimeBlock(){
-    var businessHours = ["9AM","10AM","11AM","12AM","1PM","2PM","3PM","4PM","5PM"];
+    var businessHours = ["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"];
+    var hour = date.getHours();
 
-    for(i = 0; i < businessHours.length ; i++){
-       
+    if (hour > 12 ){
+        hour = hour - 12 ;
+        time = "PM";
+    }else if(hour == 12){
+        time = "PM";
+    } 
+    else{
+        time = 'AM';
+    }
+
+    var CurrentHour = hour + time;
+    var x = businessHours.indexOf(CurrentHour);
+    
+
+    if (x == -1 && time == 'AM'){
+        //all all future
+        var isbefbus= -1
+        generateTimeBlock(businessHours,isbefbus);
+    }
+
+    if (x == -1 && time == 'PM'){
+        var isaftbus =  businessHours.length + 1;
+        generateTimeBlock(businessHours, isaftbus);
+    }
+    
+    if ( x != -1){
+      generateTimeBlock(businessHours, x);
+    }
+
+}
+
+function generateTimeBlock(businessHours, posBlock){
+
+    for(i = 0; i < businessHours.length ;i++){
        //Handle the hours
-        var lblHour = document.createElement('label');
+       var lblHour = document.createElement('label');
        $(lblHour).addClass('hour');
        $(lblHour).html(businessHours[i]);
+
 
        //handle the text area
        var textArea = document.createElement('textarea');
        $(textArea).addClass('description');
-
+       
+       //compare with the index to determine if it's in the past present or future
+       if ( i < posBlock) $(textArea).addClass('past');
+       if ( i == posBlock) $(textArea).addClass("present");
+       if ( i > posBlock) $(textArea).addClass("future");
 
        //handle the button 
        var button = document.createElement('input');
@@ -57,24 +96,11 @@ function displayTimeBlock(){
        
        $('.container').append(divRow);
     }
-
 }
 
 $( document ).ready(function() {
     displayTimeBlock();
 });
-
-// $('.container').html('<label class="time-block">9:00</label>\
-//                 <textarea class="description"> Hellllllllllllllllo</textarea>\
-//                 <button type="button" class="saveBtn"></button>');
-
-                // $('.container').html('\
-                //                             <div class="row">\
-                //                                 <label class="hour">9:00</label>\
-                //                                 <textarea class="description"> Hellllllllllllllllo</textarea>\
-                //                                 <input type="submit" class="saveBtn"></input>\
-                //                             </div>\
-                //                     ');
 
 
 
